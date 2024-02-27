0407df2f33eb8e566e5c88e5b402409b3fae868c
Krish Acme <acmekrish@gmail.com>
2024-02-27 17:12:22 +0100

    Home

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -5,7 +5,7 @@ const Solution = (props) => {
     <>
       <div className = " text-black bg-[#F1F6FF] font-space-grotesk font-Open-sans ">
       <div className={props.margin}>
-      <div className = " justify-center  flex ">
+      <div className = " justify-center flex ">
         <p className = "text-center font-bold border-0 rounded-full bg-[#9BBFF01A] px-[19px] py-[6px]">{props.main}</p>
       </div>
         <h1 className = 'font-bold md:text-[36px] text-[24px] py-[24px] justify-center text-center flex lg:leading-[48px]'>{props.sub}</h1>
@@ -13,7 +13,7 @@ const Solution = (props) => {
             <p className = "text-[18px] px-auto flex lg:w-[699px] text-center md:pb-0 pb-[60px]">{props.text}</p>
         </div>
         {/* Card */}
-        <div className = {`flex ${props.style} flex-col items-center`}>
+        <div className = {`flex ${props.style} right-0 flex-col items-center`}>
         <div className="flex flex-col">
         <div className = " align-left flex lg:pt-[64px] pt-[40px] lg:px-[120px] px-[20px]">
             <p className = "text-center font-bold border-0 rounded-full bg-[#9BBFF01A] text-[#1565D8] px-[19px] py-[6px]">{props.card}</p>
